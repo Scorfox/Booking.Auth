b5d8c4ae9c9b486a5f7eb451381c1e7e0a3d54e3
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-02-07 22:01:25 +0500

    Добавила MassTransit

--- src/Core/Booking.Auth.Application/Repositories/IUserRepository.cs
+++ src/Core/Booking.Auth.Application/Repositories/IUserRepository.cs
@@ -4,6 +4,6 @@ namespace Booking.Auth.Application.Repositories;
 
 public interface IUserRepository : IBaseRepository<User>
 {
-    Task<User?> FindByEmailAsync(bool useAsNoTracking, string email, CancellationToken token);
-    Task<bool> HasAnyByEmailAsync(string email, CancellationToken token);
+    Task<User?> FindByEmailAsync(bool useAsNoTracking, string email, CancellationToken token = default);
+    Task<bool> HasAnyByEmailAsync(string email, CancellationToken token = default);
 }
