b6904f7ef06309ed9978ce6e9b59bf2f0cf5bcf1
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-02-07 22:26:17 +0500

    renaming: client -> user

--- src/Core/Booking.Auth.Application/Consumers/User/CreateUserConsumer.cs
+++ src/Core/Booking.Auth.Application/Consumers/User/CreateUserConsumer.cs
@@ -2,39 +2,38 @@
 using Booking.Auth.Application.Common;
 using Booking.Auth.Application.Exceptions;
 using Booking.Auth.Application.Repositories;
-using Booking.Auth.Domain.Entities;
 using MassTransit;
 using Microsoft.AspNetCore.Identity;
 using Otus.Booking.Common.Booking.Contracts.Authentication.Requests;
 using Otus.Booking.Common.Booking.Contracts.Authentication.Responses;
 
-namespace Booking.Auth.Application.Consumers.Client;
+namespace Booking.Auth.Application.Consumers.User;
 
-public class CreateClientConsumer : IConsumer<CreateClient>
+public class CreateUserConsumer : IConsumer<CreateUser>
 {
     private readonly IRoleRepository _roleRepository;
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
 
-    public CreateClientConsumer(IRoleRepository roleRepository, IUserRepository userRepository, IMapper mapper)
+    public CreateUserConsumer(IRoleRepository roleRepository, IUserRepository userRepository, IMapper mapper)
     {
         _roleRepository = roleRepository;
         _userRepository = userRepository;
         _mapper = mapper;
     }
     
-    public async Task Consume(ConsumeContext<CreateClient> context)
+    public async Task Consume(ConsumeContext<CreateUser> context)
     {
         var request = context.Message;
         if (await _userRepository.HasAnyByEmailAsync(request.Email))
             throw new BadRequestException($"User with {request.Email} already exists");
             
-        var user = _mapper.Map<User>(request);
+        var user = _mapper.Map<Domain.Entities.User>(request);
         user.Role = (await _roleRepository.GetByNameAsync(Roles.Client))!;
-        user.PasswordHash = new PasswordHasher<User>().HashPassword(user, request.Password);
+        user.PasswordHash = new PasswordHasher<Domain.Entities.User>().HashPassword(user, request.Password);
         
         await _userRepository.Create(user);
 
-        await context.RespondAsync(_mapper.Map<CreateClientResult>(user));
+        await context.RespondAsync(_mapper.Map<CreateUserResult>(user));
     }
 }
