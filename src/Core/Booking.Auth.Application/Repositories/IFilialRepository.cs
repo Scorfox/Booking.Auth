80a4cb66c903550c41d823b2b99f2afc8b73b3f2
Haxend <dunaevskyilya@gmail.com>
2024-03-01 00:32:00 +0300

    feat: GetFilialIdConsumer
    + Test

--- src/Core/Booking.Auth.Application/Repositories/IFilialRepository.cs
+++ src/Core/Booking.Auth.Application/Repositories/IFilialRepository.cs
@@ -4,9 +4,9 @@ namespace Booking.Auth.Application.Repositories;
 
 public interface IFilialRepository : IBaseRepository<Filial>
 {
+    Task<bool> HasAnyWithIdAsync(Guid id, CancellationToken cancellationToken = default);
     Task<bool> HasAnyWithNameAsync(string inn, CancellationToken cancellationToken = default);
     Task<bool> HasAnyWithNameExceptIdAsync(Guid id, string inn, CancellationToken cancellationToken = default);
     Task<List<Filial>> GetFilialsListAsync(int offset, int limit, CancellationToken cancellationToken = default);
-
     Task DeleteFilialByIdAsync(Guid id, CancellationToken cancellationToken = default);
 }
